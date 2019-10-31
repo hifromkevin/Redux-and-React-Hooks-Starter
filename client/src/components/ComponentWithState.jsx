81680c1d6d32fe6e3566633408421773a6e10896
hifromkevin <lam.kevin86@gmail.com>
2019-10-31 11:50:15 -0700

    Modifies some text

--- client/src/components/ComponentWithState.jsx
+++ client/src/components/ComponentWithState.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchTweets } from '../actions';
+import { fetchInfo } from '../actions';
 
 import InnerComponent from './InnerComponent.jsx';
 
@@ -9,12 +9,12 @@ const ComponentWithState = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchTweets());
+    dispatch(fetchInfo());
   }, []);
 
   return (
-    <div className="tweets">
-      <h1>Tweets</h1>
+    <div className="infoSection">
+      <h1>Component With State</h1>
       <ul>
       {stateData.map(datas => {
         return <li><InnerComponent datas={datas} key={datas.id} /></li>
