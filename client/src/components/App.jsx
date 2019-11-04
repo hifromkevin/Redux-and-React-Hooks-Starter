e3789fafebd803e57a68a0695d72a8720590a4af
hifromkevin <lam.kevin86@gmail.com>
2019-11-04 12:55:04 -0800

    Fixes typo

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import CompoentWithState from './CompoentWithState.jsx';
+import ComponentWithState from './ComponentWithState.jsx';
 
 const App = () => {
   return (
     <div className="main">
       <h1  className="main__text">Welcome to the Redux and React Hook Starter! <FontAwesome name="star" /></h1>
-      <CompoentWithState />
+      <ComponentWithState />
     </>
   )
 
